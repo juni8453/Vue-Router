4dce740a6e750fd40ddd0f780e3249b7397383bc
ByeoungJoon Jeon <juni8453@naver.com>
2023-12-22 19:24:51 +0900

    데이터 바인딩, props 등 복습

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <Post v-for="(post, i) in posts" :key="i" :post="post" />
 
 </template>
 
 <script>
+import Post from './components/Post.vue'
+import posts from './assets/posts.js'
+
 export default {
   name: 'App',
+  data() {
+    return {
+      posts: posts,
+    }
+  },
   components: {
-
+    Post: Post,
   }
 }
 </script>
@@ -18,6 +53,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Post.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>{{post.number}}</p>
+    <h5>{{post.title}}</h5>
+    <p>{{post.content}}</p>
+    <p>{{post.date}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Post",
+  props: {
+    post: Array,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
